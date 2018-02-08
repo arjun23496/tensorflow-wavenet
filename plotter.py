f44e67802fd541951b8f5bde1cfb7a1c08fb0f95
arjun23496 <arjun.k018@gmail.com>
2018-02-08 18:29:17 -0500

    Output verification code

--- plotter.py
+++ plotter.py
@@ -5,17 +5,27 @@ import matplotlib
 matplotlib.use('GTK')
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 generated_file = sys.argv[1]
-# ground_truth = sys.argv[2]
 save_dir = sys.argv[3]
+index = int(sys.argv[4])
+count = int(sys.argv[5])
+
+# Finding ground truth
+count += 1
+
+if os.path.isfile('../data/test-corpus/p'+str(index)+"_"+str(count)+".entropy"):
+	ground_truth = np.loadtxt('../data/test-corpus/p'+str(index)+"_"+str(count)+".entropy")
+else:
+	ground_truth = np.loadtxt('../data/corpus/p'+str(index)+'/p'+str(index)+"_"+str(count)+".entropy")
+
+generated_data = np.load(generated_file)
 
 plt.plot(generated_file, label='generated')
-# plt.plot(ground_truth, label='ground truth', alpha=0.7)
-
-print generated_file
+plt.plot(ground_truth, label='ground truth', alpha=0.7)
 
 plt.legend()
 
-file_name = ground_truth.split('/')[-1]
-# plt.savefig(save_dir+'/'+file_name+'.png')
+file_name = 'p'+str(index)+"_"+str(count)+".entropy"
+plt.savefig(save_dir+'/'+file_name+'.png')
