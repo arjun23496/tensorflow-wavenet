c7e3e6d9712a8b2b67e0ff265712cdfc93a78d17
Arjun Karuvally <akaruvally@node019.cm.cluster>
2018-02-08 17:29:21 -0500

    Working testing script

--- generate.sh
+++ generate.sh
@@ -1,6 +1,10 @@
-for filename in ../data/test-corpus/*.entropy do:
-	echo "Processing : ",$filename
+for filename in ../data/test-corpus/*.entropy; do
+	echo "Processing : $filename"
+	string=$(python splitter.py $filename)
+	read -ra ADDR <<< "$string"
+	index="${ADDR[0]}"
+	count="${ADDR[1]}"
+	#python generate.py --samples 8000 --wav_out_path speaker.npy --gc_cardinality=120 --gc_channels=119 --gc_id=$($index) --wav_seed ../data/test-corpus/$($filename) logdir/train/2018-02-06T21-26-11/model.ckpt-54350
 	
-	# python generate.py --samples 8000 --wav_out_path speaker.npy --gc_cardinality=120 --gc_channels=119 --gc_id=1 --wav_seed ../data/test-corpus/$($filename) logdir/train/2018-02-06T21-26-11/model.ckpt-54350
 
 done
--- splitter.py
+++ splitter.py
@@ -4,7 +4,7 @@ string = sys.argv[1]
 
 string = string.split('/')[-1].split('_')
 
-index = string[1][1:]
-count = string.split('.')[0]
+index = string[0][1:]
+count = string[1].split('.')[0]
 
 print index," ",count
