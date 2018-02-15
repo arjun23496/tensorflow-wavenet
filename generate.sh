ccf7da3eef27b6cc62864bd7c6b69e18f6cde4b2
Arjun Karuvally <akaruvally@gypsum.cm.cluster>
2018-02-15 18:52:44 -0500

    Bug Fix - generate.py have issues generating files

--- generate.sh
+++ generate.sh
@@ -4,7 +4,7 @@ for filename in ../data/test-corpus/*.entropy; do
 	read -ra ADDR <<< "$string"
 	index="${ADDR[0]}"
 	count="${ADDR[1]}"
-	wav_out_path="results/output/speaker_p$index_$count.npy"
+	wav_out_path="results/output/speaker_p$(echo $index)_$(echo $count).npy"
 	wav_seed="$filename"
 	figure_path="results/figures"
 	echo $index
@@ -12,6 +12,6 @@ for filename in ../data/test-corpus/*.entropy; do
 	echo $wav_out_path
 	echo $wav_seed
 	echo $figure_path
-	python generate.py --samples 8000 --wav_out_path $wav_out_path --gc_cardinality=120 --gc_channels=119 --gc_id $index --wav_seed $wav_seed logdir/train/2018-02-06T21-26-11/model.ckpt-54350
+	python generate.py --samples 20000 --wav_out_path $wav_out_path --gc_cardinality=120 --gc_channels=119 --gc_id $index --wav_seed $wav_seed logdir/train/2018-02-06T21-26-11/model.ckpt-54350
 	python plotter.py $wav_out_path $figure_path $index $count 
 done
